1645462944aaeaa07eba33742759a1b1e33024e2
Sebastián <motocars.concesionaria@gmail.com>
2026-08-10 09:09:30 -0300

    Avance sección motos: Hero, categorías y base RVM

--- componentes/motos/BrandsSection.tsx
+++ componentes/motos/BrandsSection.tsx
@@ -1,15 +1,6 @@
 import "./BrandsSection.css";
 import "./RVMSection.css";
 
-export default function RVMSection() {
-  return (
-    <section className="rvm-section">
-      <div className="container">
-        <h2>RVM Patagonia</h2>
-      </div>
-    </section>
-  );
-}
 
 const categories = [
   {
---
+++ componentes/motos/RVMSection.tsx
@@ -0,0 +1,11 @@
+import "./RVMSection.css";
+
+export default function RVMSection() {
+  return (
+    <section className="rvm-section">
+      <div className="container">
+        <h2>RVM Patagonia</h2>
+      </div>
+    </section>
+  );
+}
